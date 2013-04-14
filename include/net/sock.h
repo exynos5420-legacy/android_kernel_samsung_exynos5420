77f02acc38e4da78c3ea8df5bdb733a80fed25ee
Daniel Borkmann <dborkman@redhat.com>
2013-04-14 08:08:13 +0000

    BACKPORT: net: sock: make sock_tx_timestamp void
    Currently, sock_tx_timestamp() always returns 0. The comment that
    describes the sock_tx_timestamp() function wrongly says that it
    returns an error when an invalid argument is passed (from commit
    20d4947353be, ``net: socket infrastructure for SO_TIMESTAMPING'').
    Make the function void, so that we can also remove all the unneeded
    if conditions that check for such a _non-existant_ error case in the
    output path.
    
    Change-Id: Ibdfd5071737190371d4abec5ae76046b5aa8de23
    Signed-off-by: Daniel Borkmann <dborkman@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2094,7 +2094,7 @@ static inline void sock_recv_ts_and_drops(struct msghdr *msg, struct sock *sk,
  *
  * Currently only depends on SOCK_TIMESTAMPING* flags.
  */
-extern int sock_tx_timestamp(struct sock *sk, __u8 *tx_flags);
+void sock_tx_timestamp(struct sock *sk, __u8 *tx_flags);
 
 /**
  * sk_eat_skb - Release a skb if it is no longer needed
