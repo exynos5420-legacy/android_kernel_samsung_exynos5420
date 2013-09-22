58113fcbd2b64ee6eea3ac4344467dc2838fdfb8
Joe Perches <joe@perches.com>
2013-09-22 10:32:26 -0700

    BACKPORT: sock.h: Remove extern from function prototypes
    There are a mix of function prototypes with and without extern
    in the kernel sources.  Standardize on not using extern for
    function prototypes.
    
    Function prototypes don't need to be written with extern.
    extern is assumed by the compiler.  Its use is as unnecessary as
    using auto to declare automatic/local variables in a block.
    
    Change-Id: I65041ae9f8472dc9f84f5f8e09dc3ac859c7d05a
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -811,7 +811,7 @@ void sk_stream_wait_close(struct sock *sk, long timeo_p);
 int sk_stream_error(struct sock *sk, int flags, int err);
 void sk_stream_kill_queues(struct sock *sk);
 
-int sk_wait_data(struct sock *sk, long *timeo, const struct sk_buff *skb);
+extern int sk_wait_data(struct sock *sk, long *timeo);
 
 struct request_sock_ops;
 struct timewait_sock_ops;
@@ -1432,19 +1432,26 @@ struct sk_buff *sock_rmalloc(struct sock *sk, unsigned long size, int force,
 void sock_wfree(struct sk_buff *skb);
 void sock_rfree(struct sk_buff *skb);
 
-int sock_setsockopt(struct socket *sock, int level, int op,
-		    char __user *optval, unsigned int optlen);
+extern int			sock_setsockopt(struct socket *sock, int level,
+						int op, char __user *optval,
+						unsigned int optlen);
 
-int sock_getsockopt(struct socket *sock, int level, int op,
-		    char __user *optval, int __user *optlen);
-struct sk_buff *sock_alloc_send_skb(struct sock *sk, unsigned long size,
-				    int noblock, int *errcode);
-struct sk_buff *sock_alloc_send_pskb(struct sock *sk, unsigned long header_len,
-				     unsigned long data_len, int noblock,
-				     int *errcode);
-void *sock_kmalloc(struct sock *sk, int size, gfp_t priority);
-void sock_kfree_s(struct sock *sk, void *mem, int size);
-void sk_send_sigurg(struct sock *sk);
+extern int			sock_getsockopt(struct socket *sock, int level,
+						int op, char __user *optval, 
+						int __user *optlen);
+extern struct sk_buff 		*sock_alloc_send_skb(struct sock *sk,
+						     unsigned long size,
+						     int noblock,
+						     int *errcode);
+extern struct sk_buff 		*sock_alloc_send_pskb(struct sock *sk,
+						      unsigned long header_len,
+						      unsigned long data_len,
+						      int noblock,
+						      int *errcode);
+extern void *sock_kmalloc(struct sock *sk, int size,
+			  gfp_t priority);
+extern void sock_kfree_s(struct sock *sk, void *mem, int size);
+extern void sk_send_sigurg(struct sock *sk);
 
 #ifdef CONFIG_CGROUPS
 void sock_update_classid(struct sock *sk);
@@ -2087,7 +2094,7 @@ static inline void sock_recv_ts_and_drops(struct msghdr *msg, struct sock *sk,
  *
  * Currently only depends on SOCK_TIMESTAMPING* flags.
  */
-void sock_tx_timestamp(struct sock *sk, __u8 *tx_flags);
+extern int sock_tx_timestamp(struct sock *sk, __u8 *tx_flags);
 
 /**
  * sk_eat_skb - Release a skb if it is no longer needed
