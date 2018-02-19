4c7259145dfd30b719f5bee6052d7a6ce33956e8
Florian Westphal <fw@strlen.de>
2018-02-19 01:24:15 +0100

    netfilter: ebtables: CONFIG_COMPAT: don't trust userland offsets
    commit b71812168571fa55e44cdd0254471331b9c4c4c6 upstream.
    
    We need to make sure the offsets are not out of range of the
    total size.
    Also check that they are in ascending order.
    
    The WARN_ON triggered by syzkaller (it sets panic_on_warn) is
    changed to also bail out, no point in continuing parsing.
    
    Briefly tested with simple ruleset of
    -A INPUT --limit 1/s' --log
    plus jump to custom chains using 32bit ebtables binary.
    
    Reported-by: <syzbot+845a53d13171abf8bf29@syzkaller.appspotmail.com>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    CVE-2018-1068
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    
    Change-Id: I9cdfe2f0b7f219c26080f60f99246edd8e7fe93d

--- net/bridge/netfilter/ebtables.c
+++ net/bridge/netfilter/ebtables.c
@@ -2016,6 +2016,9 @@ static int ebt_size_mwt(const struct compat_ebt_entry_mwt *match32,
 		if (match_kern)
 			match_kern->match_size = ret;
 
+		if (WARN_ON(type == EBT_COMPAT_TARGET && size_left))
+			return -EINVAL;
+
 		match32 = (struct compat_ebt_entry_mwt *) buf;
 	} while (size_left);
 
@@ -2073,12 +2076,8 @@ static int size_entry_mwt(const struct ebt_entry *entry, const unsigned char *ba
 	 * offsets are relative to beginning of struct ebt_entry (i.e., 0).
 	 */
 	for (i = 0; i < 4 ; ++i) {
-		if (offsets[i] > *total)
+		if (offsets[i] >= *total)
 			return -EINVAL;
-
-		if (i < 3 && offsets[i] == *total)
-			return -EINVAL;
-
 		if (i == 0)
 			continue;
 		if (offsets[i-1] > offsets[i])
