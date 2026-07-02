e0916e8df9310e5c09b3cac1c0b7781b11affc50
Yaroslav Zviezda <acroreiser@gmail.com>
2026-07-02 22:57:58 +0300

    net: wireless: ignore 802.11d regulatory domain hints

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -1746,6 +1746,8 @@ void regulatory_hint_11d(struct wiphy *wiphy,
 	enum environment_cap env = ENVIRON_ANY;
 	struct regulatory_request *request;
 
+	return;
+
 	mutex_lock(&reg_mutex);
 
 	if (unlikely(!last_request))
